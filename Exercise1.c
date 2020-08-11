5d8e8834df94827ae1909d88da6b13982997f571
tndq0106 <69330997+tndq0106@users.noreply.github.com>
2020-08-11 10:51:29 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -14,7 +14,23 @@
 #define SIZE 1000
 
 void ex1(char *str[],int n){
-	//your codes here
+	 if (n == 0) 
+        return ""; 
+  
+    if (n == 1) 
+        return str[0]; 
+  
+    sort(str, str + n); 
+    int len = min(str[0].size(),  
+                 str[n - 1].size()); 
+  
+    string first = str[0], last = str[n - 1]; 
+    int i = 0; 
+    while (i < len && first[i] == last[i]) 
+        i++; 
+  
+    string pre = first.substr(0, i); 
+    return pre; 
 }
 
 int main(int argc, char *argv[]) {
